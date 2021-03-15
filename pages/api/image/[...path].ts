b71cbc2815842f79e1f963b7e0ad74e2751d7825
koke-desu <gbpvp25645@yahoo.co.jp>
2021-03-15 17:09:18 +0900

    roll back changes.

--- pages/api/image/[...path].ts
+++ pages/api/image/[...path].ts
@@ -1,34 +1,19 @@
-//import { NextApiRequest, NextApiResponse } from 'next'
 import { getImage } from "../../../firebase/dbFunctions";
 import admin from "firebase-admin";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// export default async (req, res) => {
-//   const {
-//     query: { path },
-//   } = req;
-//   //const Storage: admin.storage.Storage = admin.storage();
-//   res.status(200).json({
-//     url: await getImage(path.join("/"))
-//       .then((res) => {
-//         return res;
-//       })
-//       .catch((err) => {
-//         return "image not found";
-//       }),
-//   });
-// };
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body);
-  switch (req.method) {
-    case "GET":
-      console.log("get");
-      break;
-    case "POST":
-      console.log("post");
-      break;
-    default:
-      console.log("default");
-  }
+export default async (req, res) => {
+  const {
+    query: { path },
+  } = req;
+  //const Storage: admin.storage.Storage = admin.storage();
+  res.status(200).json({
+    url: await getImage(path.join("/"))
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return "image not found";
+      }),
+  });
 };
