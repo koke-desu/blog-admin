201708e5f657883a22c3ed562e90422e6b6a12f6
koke-desu <gbpvp25645@yahoo.co.jp>
2021-03-15 23:43:30 +0900

    add image uploader and get its URL.

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,19 +9,51 @@ import { useState } from "react";
 import ReactMarkdown from "react-markdown/with-html";
 import gfm from "remark-gfm";
 import useSwr from "swr";
-import { postImage } from "../firebase/apiFunctions";
+import { postImage } from "../firebase/cliantSideFunction";
+
+// bodyの整形。以下の処理を順に行う。
+// ・改行の挿入
+// ・画像の挿入。 画像は<img ファイル名 width={幅} height={高さ} className=""/>と入力。それぞれの間に半角スペース必須。width, height, classNameは順不同。
+//   classNameでstyling出来るが必要ない場合は書かなくてもいい。
+//   この関数によって、ファイル名の部分にsrcを挿入する。 例) <img image.jps width={100} height={100} className="p-1" />
+function formatMD(body: string, imglinks: { name: string; url: string }[]) {
+  if (typeof body != "string") return "body is not string";
+  let res = body.replace(/\n/g, "\n<br>");
+
+  let from_index = 0;
+  while (true) {
+    const start = res.indexOf("<img", from_index);
+    const end = res.indexOf("/>", from_index);
+    if (start == -1 || end == -1) break;
+    const slice = res.slice(start, end).split(" ");
+    let src = "/images/no_image.png";
+    imglinks.forEach((link) => {
+      if (link.name == slice[1]) src = link.url;
+    });
+    let Image = `<img src="${src}" ${slice[2]} ${slice[3]} ${
+      slice[4] ? slice[4] : ""
+    }/>`;
+    res = res.slice(0, start) + Image + res.slice(end + 2);
+    from_index = start + Image.length;
+  }
+
+  return res;
+}
 
 export default function Home() {
   const [title, setTitle] = useState("title");
-  const [body, setBody] = useState("本文\n a \n");
-  const [imglinks, setImagelinks] = useState<string[]>([]);
+  const [body, setBody] = useState<string>("本文\n a \n");
+  const [imglinks, setImglinks] = useState<{ name: string; url: string }[]>([]);
 
-  postImage("path", "--------body---------");
-
+  // 画像のアップロードを行い、imglinksに画像へのリンクを追加する。
   function uploadHandle(event: React.ChangeEvent<HTMLInputElement>) {
     event.preventDefault();
-
     const file: File = event.target.files[0];
+    postImage(`test/${file.name}`, file).then((url) => {
+      const links = imglinks.slice();
+      links.push({ name: file.name, url });
+      setImglinks(links);
+    });
   }
 
   return (
@@ -37,14 +69,19 @@ export default function Home() {
               {imglinks.map((link, index) => {
                 return (
                   <div>
-                    <Image src={link} height={30} width={30} />
-                    <p>{index}</p>
+                    <Image
+                      src={link.url}
+                      height={30}
+                      width={30}
+                      key={`img-${index}-${link.name}`}
+                    />
+                    <p>{link.name}</p>
                   </div>
                 );
               })}
               <label>
-                <input type="file" onChange={uploadHandle} />
                 画像をアップロード
+                <input type="file" onChange={uploadHandle} />
               </label>
             </div>
             <input
@@ -55,15 +92,14 @@ export default function Home() {
               }}
             />
             <textarea
-              cols={70}
+              cols={50}
               rows={100}
               onChange={(eve) => {
                 setBody(eve.currentTarget.value);
               }}
               className="border-0 p-1"
-            >
-              {body}
-            </textarea>
+              value={body}
+            />
           </div>
           <div className="w-1/2">
             <div
@@ -93,8 +129,7 @@ export default function Home() {
                 })} */}
               </div>
               <ReactMarkdown plugins={[[gfm]]} allowDangerousHtml>
-                {/* <br/>を使って改行を表示する。\nがないとmarkdownがうまいこと適応できないので\nもつける。 */}
-                {body}
+                {formatMD(body, imglinks)}
               </ReactMarkdown>
             </div>
           </div>
